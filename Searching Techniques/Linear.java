ac9d808f7d9ca4979728c1b3f48271e08361b9a9
durgeshbachhav <bachhavdurgesh123@gmail.com>
2023-10-01 17:57:34 +0530

    linear search

---
+++ Searching Techniques/Linear.java
@@ -0,0 +1,24 @@
+class Linear{
+     public static void main(String[] args) {
+          
+          // linear search 
+          // it is searching technique
+          // it use to find particular element in array
+          
+          // pseudocode
+          /*
+               for each element in the list/array:
+               if element == target:
+                    return index
+               return not found
+           */
+
+          int arr[] = {2,3,4,5,6,7,89,4};
+          int target = 9;
+           for(int i = 0 ;i< arr.length;i++){
+               if(arr[i] == target){
+                    System.out.println("element found at index : "+ i);
+               } 
+           }
+     }
+}
